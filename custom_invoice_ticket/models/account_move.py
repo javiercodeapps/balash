64e0495b74f73a526cf728a836ab2a352298ed71
Javier Pepe <javierpepe@gmail.com>
2026-08-12 20:32:00 -0300

    pos invoice

--- custom_invoice_ticket/models/account_move.py
+++ custom_invoice_ticket/models/account_move.py
@@ -5,7 +5,7 @@ class AccountMove(models.Model):
     _inherit = 'account.move'
 
     def action_invoice_download_pdf(self, target="download"):
-        if self.l10n_ar_is_pos:
+        if self.journal_id.l10n_ar_is_pos:
             report = self.env['account.move.send']._get_default_pdf_report_id(self)
             if report:
                 return report.with_context(
