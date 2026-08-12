1c5b73945777631583588ce3a267fb68eeee838c
Javier Pepe <javierpepe@gmail.com>
2026-08-12 20:35:49 -0300

    pos invoice

--- custom_invoice_ticket/models/account_move_send.py
+++ custom_invoice_ticket/models/account_move_send.py
@@ -9,12 +9,12 @@ class AccountMoveSend(models.AbstractModel):
         if journal.l10n_ar_is_pos:
             if journal.l10n_ar_afip_pos_system == 'II_IM':
                 report = self.env.ref(
-                    'custom_invoice_ticket.action_report_invoice_ticket',
+                    'custom_invoice_ticket.action_report_invoice_ticket_b',
                     raise_if_not_found=False,
                 )
             else:
                 report = self.env.ref(
-                    'custom_invoice_ticket.action_report_invoice_ticket_b',
+                    'custom_invoice_ticket.action_report_invoice_ticket',
                     raise_if_not_found=False,
                 )
             if report and move._is_action_report_available(report):
