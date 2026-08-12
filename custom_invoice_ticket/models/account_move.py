a61d32d1f7c5a2a10f1b2b4804763820a505671e
Javier Pepe <javierpepe@gmail.com>
2026-08-12 20:21:35 -0300

    pos invoice

---
+++ custom_invoice_ticket/models/account_move.py
@@ -0,0 +1,14 @@
+from odoo import models
+
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    def action_invoice_download_pdf(self, target="download"):
+        if self.l10n_ar_is_pos:
+            report = self.env['account.move.send']._get_default_pdf_report_id(self)
+            if report:
+                return report.with_context(
+                    skip_invoice_sync=True,
+                ).report_action(self.id, config=False)
+        return super().action_invoice_download_pdf(target=target)
